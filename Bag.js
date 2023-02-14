77a7d25fbfa8a7054873360eeba907a7ba2ba4e1
Faris-07 <farisuddin03@gmail.com>
2023-02-14 10:30:33 +0000

    Add Bag class

--- Bag.js
+++ Bag.js
@@ -1,10 +1,15 @@
 class Bag {
+    static maximumWeight = 23;
+  
     constructor(weight) {
-        if(!weight){
-            throw new Error('bag needs weight')
-        } 
-        this.weight = weight
+      if (!weight) {
+        throw new Error("bag needs weight");
+      }
+      if (weight > Bag.maximumWeight) {
+        throw new Error("bag exceeds maximum weight");
+      }
+      this.weight = weight;
     }
-}
+  }
 
 module.exports = Bag
